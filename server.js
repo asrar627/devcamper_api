e1ee7a2395b7a33192b5efa64f2a7599dbff595c
Asrar Ahmad <asrar627>
2022-07-21 18:40:15 +0500

    some initial practice before bootcamps crud operation

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const dotenv =  require('dotenv');
 dotenv.config({path: './config/config.env'}) // we are giving the path where to look
 
 const app = express();
+app.get('/', (req, res) => {
+    // res.send( { name: "Asrar" } ); // here express auto convert into json by detecting
+    // res.json( { name: "Asrar" } ); // this is specifically for json 
+    // res.sendStatus(400); // just send status 
+    // res.status(400).json({success: false});
+    res.status(200).json({success: true, data: {name: "Asrar"}});
+});
 const PORT = process.env.PORT || 5000
 
 // For run server
