552ab802e6fa19c33db3bbf45fdd97f20beb28b2
Asrar Ahmad <asrar627>
2022-08-10 17:21:32 +0500

    add user association into course

--- controller/courses.js
+++ controller/courses.js
@@ -48,13 +48,16 @@ exports.getCourse = asyncHandler(async(req, res, next) => {
 
 exports.addCourse = asyncHandler(async(req, res, next) => {
     req.body.bootcamp = req.params.bootcampId;
-    console.log(req.body)
-    const bootcamp = Bootcamp.findById(req.params.bootcampId);
+    req.body.user = req.user.id;
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
     if (!bootcamp){
         return next(new ErrorResponse(`No Bootcamp with the id of ${req.params.bootcampId}`), 404);
     }
-
+    // Make sure user is bootcamp owner
+    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next( new ErrorResponse(`User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`, 401));
+    };
     const course = await Course.create(req.body);
     res.status(200).json({
         success: true,
@@ -68,11 +71,16 @@ exports.addCourse = asyncHandler(async(req, res, next) => {
 
 exports.updateCourse = asyncHandler(async(req, res, next) => {
     // Here variable made with let type because we need it to update
-    let course = Course.findById(req.params.id);
+    let course = await Course.findById(req.params.id);
 
     if (!course){
         return next(new ErrorResponse(`No Course with the id of ${req.params.id}`), 404);
     }
+
+    // Make sure user is course owner
+    if (course.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next( new ErrorResponse(`User ${req.user.id} is not authorized to update a course ${course._id}`, 401));
+    };
 
     course = await Course.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
@@ -90,11 +98,16 @@ exports.updateCourse = asyncHandler(async(req, res, next) => {
 
 exports.deleteCourse = asyncHandler(async(req, res, next) => {
     // Here variable made with let type because we dont need it to update
-    const course = Course.findById(req.params.id);
+    const course = await Course.findById(req.params.id);
 
     if (!course){
         return next(new ErrorResponse(`No Course with the id of ${req.params.id}`), 404);
     }
+
+    // Make sure user is course owner
+    if (course.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next( new ErrorResponse(`User ${req.user.id} is not authorized to delete a course ${course._id}`, 401));
+    };
 
     await course.remove();
 
