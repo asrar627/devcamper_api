caf95178d265b4a73e48c3ad5aaf7ffec4e691e3
Asrar Ahmad <asrar627>
2022-08-15 16:53:01 +0500

    using aggregation save rating into bootcamp using users reviews

--- model/Review.js
+++ model/Review.js
@@ -36,4 +36,45 @@ const ReviewSchema = new mongoose.Schema({
 // Prevent User from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1}, {unique: true} );
 
+// Static method to get avg rating and save
+ReviewSchema.statics.getAverageRating = async function(bootcampId) {
+    console.log("Calculating average cost...".blue);
+
+    const obj = await this.aggregate([
+        {
+            $match: {
+                bootcamp: bootcampId 
+            }
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageRating: {
+                    $avg: '$rating'
+                }
+            }
+        }
+    ]);
+    console.log(obj);
+    try{
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageRating: obj[0].averageRating
+        });
+    }catch(err){
+        console.error(err);
+    }
+}
+
+
+// Call getAverageCost after Save
+ReviewSchema.post('save', function(){
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageCost before Remove
+ReviewSchema.pre('remove', function(){  
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
+
 module.exports = mongoose.model('Review', ReviewSchema);
